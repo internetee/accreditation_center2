d94f09a600d5b85232f37dc485981eadc43ddfa2
Sergei Tsoganov <sergei@Sergeis-MacBook-Pro.local>
2025-11-27 16:21:09 +0200

    Formatted cookie consent and fixed mobile menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "cookieconsent-config"
 import '@popperjs/core'
 import Header from "src/header"
 import Select from "src/select"
