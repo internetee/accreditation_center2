e651f6a910cb54c572189e6a351247c809f5e952
mmeest <martin.maasik.001@mail.ee>
2025-12-09 10:35:18 +0200

    update

--- app/controllers/admin/test_attempts_controller.rb
+++ app/controllers/admin/test_attempts_controller.rb
@@ -24,7 +24,7 @@ class Admin::TestAttemptsController < Admin::BaseController
   end
 
   def show
-    @question_responses = @test_attempt.question_responses.includes(:question)
+    @question_responses = @test_attempt.question_responses.includes(:question, :answers)
   end
 
   def reassign
